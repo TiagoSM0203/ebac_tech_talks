50ca44fb5034fa037b07f5ae8c958bd2895272af
Tiago <tiago@teste.com>
2025-01-09 19:44:38 -0300

    finalizando o projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('ola mundo')
+AOS.init();
+
+const dataDoEvento = new Date('Jan 12, 2025 19:00:00')
+const timeStampDoEvento = dataDoEvento.getTime()
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date()
+    const timeStampAtual = agora.getTime()
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual
+
+    const diasEmMs = 1000 * 60 * 60 * 24
+    const horaEmMs = 1000 * 60 * 60
+    const minutoEmMS = 1000 * 60
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs)
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasAteOEvento) / horaEmMs)
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMS)
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMS) / 1000)
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras)
+        document.getElementById('contador').innerHTML = 'Evento expirado'
+    }
+    
+}, 1000)
